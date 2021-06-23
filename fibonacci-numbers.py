1c51ccf8fff775e8def27e6943d098e64bacd824
Gaurav Mishra <67703653+Gaurav00206@users.noreply.github.com>
2021-06-23 09:44:33 +0530

    Add files via upload

---
+++ fibonacci-numbers.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+FibArray = [0, 1]
+ 
+def fibonacci(n):
+    if n<0:
+        print("Incorrect input")
+    elif n<= len(FibArray):
+        return FibArray[n-1]
+    else:
+        temp_fib = fibonacci(n-1)+fibonacci(n-2)
+        FibArray.append(temp_fib)
+        return temp_fib
+ 
+print(fibonacci(9))
+ 
+
+
+# In[ ]:
+
+
+
+
